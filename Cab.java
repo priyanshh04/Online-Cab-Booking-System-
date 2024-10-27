f8f7436a69d171221fe01d381c4684f38130684d
Priyansh <priyanshgangwar05@gmail.com>
2024-10-27 22:08:56 +0530

    Update Cab.java

--- Cab.java
+++ Cab.java
@@ -1,1 +1,15 @@
+package model;
 
+public class Cab {
+    private String cabId;
+    private String driverName;
+    private String status; // Available or Unavailable
+
+    public Cab(String cabId, String driverName, String status) {
+        this.cabId = cabId;
+        this.driverName = driverName;
+        this.status = status;
+    }
+
+    // Getters and Setters
+}
