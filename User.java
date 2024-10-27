3e2b5a720f46524740f8d6ce7090f47300729ab1
Priyansh <priyanshgangwar05@gmail.com>
2024-10-27 22:10:12 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,15 @@
+package model;
+
+public class User {
+    private String userId;
+    private String name;
+    private String contact;
+
+    public User(String userId, String name, String contact) {
+        this.userId = userId;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    // Getters and Setters
+}
